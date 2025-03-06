d48852ce7962d5cd986452b68981970bd6cac213
michelleguerrero <michellegue8@gmail.com>
2025-03-05 23:58:14 -0800

    test

---
+++ src/main/java/com/example/beauty_store_spring_boot/BeautyStoreSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.beauty_store_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyStoreSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyStoreSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beauty_store_spring_boot/BeautyStoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beauty_store_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyStoreSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
